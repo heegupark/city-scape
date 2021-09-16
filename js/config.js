12e6be9291372f07683bf612cff12367c154d3c9
heegupark <heegu@withving.com>
2021-09-15 20:06:20 -0700

    Updated gitignore

---
+++ js/config.js
@@ -0,0 +1,17 @@
+var OPEN_WEATHER_API_KEY = '04f4c3b048013ee766a8bfee23cb8ea0'
+var PIXABAY_API_KEY = '13390108-bda3baa35dd70ef6d86c74840'
+var GEOSERVICE_API_KEY = '13140920cddf63a34f1865f5ee32d47d'
+var GEOSERVICE_APP_ID = 'b534716a'
+var GOOGLE_MAP_API_KEY = 'AIzaSyAC9Fi6o6spd5c8WnclK_FJj1z1wfkl26g'
+
+//Creat script tag for google map
+// function createScriptTag(tag, src) {
+//   var scriptTag = document.createElement(tag)
+//   scriptTag.src = src
+//   document.body.append(scriptTag)
+
+//   initAutocomplete()
+// }
+
+// var googleApiSrc = 'https://maps.googleapis.com/maps/api/js?key=' + GOOGLE_MAP_API_KEY + '&libraries=places'
+// createScriptTag('script', googleApiSrc)
